062887400d3bf72ff89f9338493044ff3d9620ac
Mirko <18010385@student.hhs.nl>
2021-10-11 15:55:04 +0200

    Histogram toegevoegd

--- Dashboard.py
+++ Dashboard.py
@@ -10,16 +10,41 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 import Get_data_api
+import Figuren
 
-st.set_page_config(layout="wide")
+# Enkele standaard opties
+st.set_page_config(layout="wide") # Zorgt voor default wide op streamlit
+pd.set_option('display.max_columns', None) # Print alles van de DataFrame pandas
 
+
+
+# Maak een titel
 st.title('Analyse Elektrische auto s')
 
 
 
 response_dataframe = Get_data_api.OpenChargeMap()
 
-pd.set_option('display.max_columns', None)
-response_dataframe#.head()
 
-response_dataframe.iloc[1]["Connections"]#[0]#["ConnectionTypeID"]
+# response_dataframe#.head()
+
+# response_dataframe.iloc[1]["Connections"]#[0]#["ConnectionTypeID"]
+
+
+
+
+
+# Sjoerd zijn code
+
+# laadpaal_data = Get_data_api.load_csv_laadpaal_data('D:/Documents/Pycharm/Case_opdrachten/Case3-elektrische-auto/laadpaaldata.csv')
+try:
+    laadpaal_data = Get_data_api.load_csv_laadpaal_data('laadpaaldata.csv')
+    st.write("Using short path")
+except:
+    laadpaal_data = Get_data_api.load_csv_laadpaal_data(
+        'D:/Documents/Pycharm/Case_opdrachten/Case3-elektrische-auto/laadpaaldata.csv')
+    st.write("Using full path")
+
+fig = Figuren.histogram(laadpaal_data)
+
+st.plotly_chart(fig)

83e4fdf20e96a303395dc072a18a6cc06af5341f
Mirko <18010385@student.hhs.nl>
2021-10-11 15:04:17 +0200

    Dataframe toegevoegd

--- Dashboard.py
+++ Dashboard.py
@@ -9,9 +9,17 @@ import time
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+import Get_data_api
 
 st.image(
     "https://i.ibb.co/R79T9ct/Naamloos.png",
     # Manually Adjust the width of the image as per requirement
 )
 st.title('Analyse Elektrische auto s')
+
+
+
+response_dataframe = Get_data_api.OpenChargeMap()
+
+pd.set_option('display.max_columns', None)
+response_dataframe#.head()

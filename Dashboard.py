3b93bd665b63dcc8e6bc8fce2b82696672e4b661
Mirko <18010385@student.hhs.nl>
2021-10-11 20:16:41 +0200

    Kleine update

--- Dashboard.py
+++ Dashboard.py
@@ -23,10 +23,10 @@ pd.set_option('display.max_columns', None) # Print alles van de DataFrame pandas
 try:
     max_results = st.secrets["max_results"]
 except:
-    max_results = 50
+    max_results = 500
 
 # Maak een titel
-st.title('Analyse Elektrische auto s')
+st.title('Dashboard elektrische auto\'s')
 
 
 # Get data from API / CSV
@@ -36,35 +36,22 @@ laadpaal_data = Get_data.load_csv_laadpaal_data('laadpaaldata.csv')
 
 
 
-
+m, bar = Figuren.map(response_dataframe, max_results)
 col1, col2 = st.columns(2)
 
 # ---------- Histogram van laadtijd ----------
 col1.plotly_chart(Figuren.histogram_laadtijd_elek_auto(laadpaal_data))
 
 
+
 # ---------- Voeg de map van locaties toe ----------
-m, bar = Figuren.map(response_dataframe, max_results)
+bar.progress(99)
 with col2:
     folium_static(m)
 bar.progress(100)
 bar.empty()
 
-# 'Starting a long computation...'
-#
-# # Add a placeholder
-# latest_iteration = st.empty()
-# bar = st.progress(0)
-# i=0
-# for j in range(100):
-#   # Update the progress bar with each iteration.
-#   latest_iteration.text(f'Iteration {i+1}')
-#   bar.progress(i + 1)
-#   time.sleep(0.1)
-#   i += 1
-#
-# '...and now we\'re done!'
-# bar.empty()
+''
 
 
 

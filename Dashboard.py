6e1d0df38ffa336921c216242b306c1b5e372e6d
Mirko <18010385@student.hhs.nl>
2021-10-12 20:52:26 +0200

    Wijziging in de layout

--- Dashboard.py
+++ Dashboard.py
@@ -23,37 +23,55 @@ pd.set_option('display.max_columns', None) # Print alles van de DataFrame pandas
 try:
     max_results = st.secrets["max_results"]
 except:
-    max_results = 500
+    max_results = 50
 
 # Maak een titel
 st.title('Dashboard elektrische auto\'s')
 
 
 # Get data from API / CSV
-response_dataframe = Get_data.OpenChargeMap(max_results)
+
 laadpaal_data = Get_data.load_csv_laadpaal_data('laadpaaldata.csv')
 
 
 
 
-m, bar = Figuren.map_folium(response_dataframe, max_results)
 
-col1, col2 = st.columns(2)
+
+
 
 # ---------- Histogram van laadtijd ----------
-col1.plotly_chart(Figuren.histogram_laadtijd_elek_auto(laadpaal_data))
+col1, col2 = st.columns([1,2])
+col1.write("Hier nog een tekst of interactie")
+col2.plotly_chart(Figuren.histogram_laadtijd_elek_auto(laadpaal_data))
 
 
 
+
+st.markdown("""---""")
 # ---------- Voeg de map van locaties toe ----------
+st.header("Waar bevinden openbare laadstations van Open Chargemap")
+col1, col2 = st.columns([1,2])
+response_dataframe = Get_data.OpenChargeMap(col1, max_results)
+m, bar = Figuren.map_folium(response_dataframe, max_results)
 bar.progress(99)
 with col2:
     folium_static(m)
 bar.progress(100)
 bar.empty()
 
+
+
+
+st.markdown("""---""")
+# ---------- Voeg de map van locaties toe ----------
+col1, col2 = st.columns([1,2])
+col1.write("Hier nog een tekst of interactie")
 autos_per_maand_cum = Get_data.rdw_data()
 fig = Figuren.lijn(autos_per_maand_cum)
-st.plotly_chart(fig)
+col2.plotly_chart(fig)
+
+
+
 
 
